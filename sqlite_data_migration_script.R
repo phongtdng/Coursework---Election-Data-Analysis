647847f37d37a0d01648649b7698165509b49ac4
phongtdng <phongtdng@gmail.com>
2024-01-16 15:52:00 +0100

    Add a new table

--- sqlite_data_migration_script.R
+++ sqlite_data_migration_script.R
@@ -9,11 +9,20 @@ cod_mun <- read_csv(file = "./data/cod_mun.csv")
 surveys <- read_csv(file = "./data/historical_surveys.csv")
 abbrev <- read_csv(file = "./data/siglas.csv")
 
+
+
 #Pivoted election_data table name: election_data
 election_df <- election_data %>% 
   pivot_longer(cols = !tipo_eleccion:votos_candidaturas, names_to = 'party', values_to = 'vote_count') %>% 
   mutate(vote_count = replace_na(vote_count, 0),
          cod_mun = paste(codigo_ccaa, codigo_provincia, codigo_municipio, sep = "-"), .after = codigo_municipio)
+
+#Municipal population table
+municipal_population <- election_data %>% 
+  mutate(cod_mun = paste(codigo_ccaa, codigo_provincia, codigo_municipio, sep = "-"), .after = codigo_municipio) %>% 
+  select(anno, cod_mun, censo) %>% 
+  summarise(sum(censo), .by = c(cod_mun, anno)) %>% 
+  rename('censo_total' = 'sum(censo)')
 
 #Pivoted surveys table name: surveys
 surveys_df <- surveys %>% 
@@ -24,9 +33,11 @@ surveys_df <- surveys %>%
 
 #Migrate data to sqlite database
 db_file <- "database.sqlite"
-db <- src_sqlite(db_file, create = TRUE) #Create database
+src_sqlite(db_file, create = TRUE) #Create database
+db <- DBI::dbConnect(SQLite(), "database.sqlite", extended_types = T) #Connect to sqlite database
 
 dbWriteTable(db, "election_data", election_df)
+dbWriteTable(db, "municipal_population", municipal_population)
 dbWriteTable(db, "cod_mun", cod_mun)
 dbWriteTable(db, "surveys", surveys_df, field.types = c(date_elec = "Date", field_date_from ="Date", field_date_to = "Date"))
 dbWriteTable(db, "abbrev", abbrev)
